84b86a3959ab16939ad0fb32e7d27540a14e497d
Vijay Saharan <105264602+vijaysaharan@users.noreply.github.com>
2022-08-30 08:15:15 +0530

    Add files via upload

---
+++ Pattern1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=0;
+    while(i<n){
+        int j=0;
+        while(j<n){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
---
+++ pattern3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=0;
+    while(i<n){
+        int j=0;
+        while(j<n){
+            cout<<j+1<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}   
